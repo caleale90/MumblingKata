ff1548f744806306c8e48bc85d94fd9f6d8fa501
Team Minerva <minerva@trovaprezzi.it>
2020-01-10 16:21:24 +0100

    * removed comments in test file

--- test/mumbling_letters_builder_test.rb
+++ test/mumbling_letters_builder_test.rb
@@ -3,33 +3,22 @@ require_relative '../mumbling_letter_builder'
 
 class MumblingLettersBuilderTest < Test::Unit::TestCase
 
-  # mumble_letters('')
-  # => ''
   def test_empty_string
     assert_equal '', MumblingLettersBuilder.new('').build
   end
 
-  # mumble_letters('a')
-  # => 'A'
   def test_single_char_string
     assert_equal 'A', MumblingLettersBuilder.new('a').build
   end
 
-  # mumble_letters('abC')
-  # => 'A-Bb-Ccc'
   def test_multiple_chars_string
     assert_equal 'A-Bb-Ccc', MumblingLettersBuilder.new('abc').build
   end
 
-  #
-  # mumble_letters('aBCd')
-  # => 'A-Bb-Ccc-Dddd'
   def test_chars_string_with_up_down
     assert_equal 'A-Bb-Ccc-Dddd', MumblingLettersBuilder.new('aBCd').build
   end
 
-  # mumble_letters('QWERTY')
-  # => 'Q-Ww-Eee-Rrrr-Ttttt-Yyyyyy'
   def test_chars_string_upcase
     assert_equal 'Q-Ww-Eee-Rrrr-Ttttt-Yyyyyy', MumblingLettersBuilder.new('QWERTY').build
   end
